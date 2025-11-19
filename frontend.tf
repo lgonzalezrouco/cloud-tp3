8de8ad6f30cfeb10b9c891b5df4f38b5f99d75c9
Emilio Neme <68005487+EmiN364@users.noreply.github.com>
2025-11-19 17:04:04 -0300

    fix build front bash

--- frontend.tf
+++ frontend.tf
@@ -10,7 +10,7 @@ resource "null_resource" "build_frontend" {
   provisioner "local-exec" {
     # Detectar sistema operativo y usar el script apropiado
     # For direct authentication: passing user pool ID and client ID (domain and redirect URI not needed)
-    command     = substr(pathexpand("~"), 0, 1) == "/" ? "bash build_frontend.sh ${module.backend_load_balancer.dns_name}/api https://${module.s3_images_bucket.bucket_id}.s3.${var.aws_region}.amazonaws.com ${aws_cognito_user_pool.matchmarket_user_pool.id} ${aws_cognito_user_pool_client.matchmarket_spa_client.id}" : "build_frontend.bat ${module.backend_load_balancer.dns_name}/api https://${module.s3_images_bucket.bucket_id}.s3.${var.aws_region}.amazonaws.com ${aws_cognito_user_pool.matchmarket_user_pool.id} ${aws_cognito_user_pool_client.matchmarket_spa_client.id}"
+    command     = substr(pathexpand("~"), 0, 1) == "/" ? "bash build_frontend.sh '${module.backend_load_balancer.dns_name}/api' 'https://${module.s3_images_bucket.bucket_id}.s3.${var.aws_region}.amazonaws.com' '${aws_cognito_user_pool.matchmarket_user_pool.id}' '${aws_cognito_user_pool_client.matchmarket_spa_client.id}'" : "build_frontend.bat ${module.backend_load_balancer.dns_name}/api https://${module.s3_images_bucket.bucket_id}.s3.${var.aws_region}.amazonaws.com ${aws_cognito_user_pool.matchmarket_user_pool.id} ${aws_cognito_user_pool_client.matchmarket_spa_client.id}"
     interpreter = substr(pathexpand("~"), 0, 1) == "/" ? ["bash", "-c"] : ["cmd", "/C"]
   }
 
